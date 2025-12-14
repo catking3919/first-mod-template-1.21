61a63407ea5b841f540acce7ae047af6a869a32e
CatGod <acatgod20@gmail.com>
2025-12-13 20:05:35 -0500

    added rat item

--- src/main/java/net/catgod/firstmod/FirstMod.java
+++ src/main/java/net/catgod/firstmod/FirstMod.java
@@ -1,5 +1,6 @@
 package net.catgod.firstmod;
 
+import net.catgod.firstmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class FirstMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/catgod/firstmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.catgod.firstmod.item;
+
+import net.catgod.firstmod.FirstMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item RAT = regesterItem("rat", new Item(new Item.Settings()));
+
+
+
+    public static Item regesterItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(FirstMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        FirstMod.LOGGER.info("Registering Mod Items for " + FirstMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(RAT);
+        });
+    }
+}
